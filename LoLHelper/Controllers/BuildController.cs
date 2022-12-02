ed0f675c5d9e7a6a770b0361d200d212a4690771
difensore <aledima0209@gmail.com>
2022-12-03 01:06:38 +0200

    some fix is added

---
+++ LoLHelper/Controllers/BuildController.cs
@@ -0,0 +1,20 @@
+﻿using LoLHelper.Interfaces;
+using LoLHelper.Models;
+using LoLHelper.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+namespace LoLHelper.Controllers
+{
+    public class BuildController:Controller
+    {
+        private readonly IDataProvider _dataprovider;
+        public BuildController(IDataProvider datapovider)
+        {
+            
+            _dataprovider = datapovider;
+        }
+
+    }
+}
